f54538a58f6a005eca75455cb786696fb1df5bb4
Guru Mahan <iosdeve2022@gmail.com>
2023-04-03 17:55:13 +0530

    whitespace remove

--- AttendenceReport/AttendanceView.swift
+++ AttendenceReport/AttendanceView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 //MARK: -> attendanceReportView
 struct AttendanceView: View {
-   //MARK: -> stateVariable
+    //MARK: -> stateVariable
     @State var firstbutton: Bool = false
     @State var secondbutton: Bool = false
     @State var thirdbutton: Bool = false
@@ -23,7 +23,6 @@ struct AttendanceView: View {
             VStack {
                 HStack{
                     Button{
-                        
                     } label: {
                         Image(systemName:"chevron.backward").foregroundColor(Color.white)
                             .font(Font.system(size: 25))
@@ -32,17 +31,14 @@ struct AttendanceView: View {
                         .foregroundColor(.white)
                         .padding(-11)
                     Spacer()
-                    
                     HStack{
                         searchButtonView
-                           
                             .frame(width: 40 ,height: 40)
                             .background(Color.white)
                     }
                     .cornerRadius(20)
                 }
                 .padding()
-                
                 VStack {
                     Label("P03 introducation to Anatomy", systemImage: "")
                         .foregroundColor(.white)
@@ -51,17 +47,13 @@ struct AttendanceView: View {
                 }
                 .padding(.leading,-100)
                 HStack {
-                   
                     completeButtonView
                     toStartButtonView
                     inProgressButtonView
                     allButtonView
-                  
                 }
-                
                 .padding(.leading,0)
                 Spacer()
-                
                 ZStack{
                     backgroundView
                         .frame(width: 400,height: 550)
@@ -72,11 +64,10 @@ struct AttendanceView: View {
             }
         }
     }
-  
+    
     //MARK: ->searchButtonView
     @ViewBuilder var searchButtonView: some View{
         Button{
-            
         } label: {
             Image(systemName:"magnifyingglass").foregroundColor(Color.blue)
                 .cornerRadius(50)
@@ -84,47 +75,34 @@ struct AttendanceView: View {
         }
         .background(Color.white)
         .frame(width: 40,height: 40)
-     }
+    }
     
     //MARK: ->completeButtonView
     @ViewBuilder var completeButtonView: some View{
-        
-            Button(action: {
-                firstbutton.toggle()
-                firstbutton = true
-                secondbutton = false
-                thirdbutton = false
-                fourthbutton = false
-                
-                
-            }) {
-                Text("Completed").padding()
-                    .font(Font.system(size: 12))
-                    .background(firstbutton ? Color.white : Color(hex: "#c0c0c0"))
-                //(hex: "#c0c0c0")
-                    .foregroundColor(Color(uiColor: .black))
-                    .frame(height: 40)
-                    .cornerRadius(20)
-                
-                
-            }
-            
-        
-        
-     }
-   
+        Button(action: {
+            firstbutton.toggle()
+            firstbutton = true
+            secondbutton = false
+            thirdbutton = false
+            fourthbutton = false
+        }) {
+            Text("Completed").padding()
+                .font(Font.system(size: 12))
+                .background(firstbutton ? Color.white : Color(hex: "#c0c0c0"))
+                .foregroundColor(Color(uiColor: .black))
+                .frame(height: 40)
+                .cornerRadius(20)
+        }
+    }
+    
     //MARK: ->toStartButtonView
     @ViewBuilder var toStartButtonView: some View{
-       
         Button(action: {
             secondbutton.toggle()
             secondbutton = true
             firstbutton = false
             thirdbutton = false
             fourthbutton = false
-            
-            
-            
         }) {
             Text("To Start")
                 .padding()
@@ -134,76 +112,55 @@ struct AttendanceView: View {
                 .frame(height: 40)
                 .background(Color(hex: "#c0c0c0"))
                 .cornerRadius(20)
-            
         }
-            
-        
-     }
-  
+    }
+    
     //MARK: ->inProgressButtonView
-
     @ViewBuilder var inProgressButtonView: some View{
-       
-        
         Button(action: {
             thirdbutton.toggle()
             firstbutton = false
             secondbutton = false
             thirdbutton = true
             fourthbutton = false
-            
         }) {
             Text("In  Progress").padding()
                 .foregroundColor(Color(uiColor: .black))
                 .font(Font.system(size: 12))
                 .background(thirdbutton ? Color.white : Color(hex: "#c0c0c0"))
-                
-            .frame(height: 40)
-            .cornerRadius(20)
-                           
-            
-               
+                .frame(height: 40)
+                .cornerRadius(20)
         }
-        
-        
-     }
-   
+    }
+    
     //MARK: ->allButtonView
     @ViewBuilder var allButtonView: some View{
-       
         Button(action: {
             fourthbutton.toggle()
             firstbutton = false
             secondbutton = false
             thirdbutton = false
             fourthbutton = true
-            
         }) {
             Text("All").padding()
                 .foregroundColor(Color(uiColor: .black))
                 .font(Font.system(size: 12))
                 .background(fourthbutton ? Color.white : Color(hex: "#c0c0c0"))
-                
                 .frame(height: 40)
-               .cornerRadius(20)
-            
-            
+                .cornerRadius(20)
         }
-       
-            
-        
-     }
-    //MARK: ->backgroundView
-   @ViewBuilder var backgroundView: some View{
-       frontView
-           .frame(width: 350,height: 500)
-           .background(Color(hex: "#c0c0c0"))
-           .padding()
-       
     }
+    
+    //MARK: ->backgroundView
+    @ViewBuilder var backgroundView: some View{
+        frontView
+            .frame(width: 350,height: 500)
+            .background(Color(hex: "#c0c0c0"))
+            .padding()
+    }
+    
     //MARK: ->popUpView
     @ViewBuilder var popUpView: some View{
-        
         VStack{
             HStack{
                 VStack{
@@ -214,27 +171,19 @@ struct AttendanceView: View {
                         .foregroundColor(.gray)
                         .padding(.leading,10)
                         .padding(.top,-3)
-                   
-                    
                 }
                 Spacer()
-                
                 Button {
-                    
                 } label: {
                     Label("", systemImage: "xmark")
                         .foregroundColor(.black)
                         .padding()
                 }
-                
             }
-            
             Divider()
-            
             HStack{
                 VStack{
                     Button {
-                        
                     } label: {
                         VStack{
                             Label("Nearby student - For all",systemImage: "")
@@ -247,65 +196,44 @@ struct AttendanceView: View {
                                 .padding(.top,-3)
                         }
                     }
-
-                    
-                   
-                    
                 }
                 Spacer()
-                
                 Button {
-                    
                 } label: {
                     Label("", systemImage: "chevron.forward")
                         .foregroundColor(.black)
                         .padding()
                 }
-                
             }
-            
             Divider()
-            
             HStack{
                 VStack{
                     Button {
-                        
                     } label: {
                         VStack{
                             Label("Nearby student - For Absentees",systemImage: "")
                                 .foregroundColor(.black)
                                 .padding(.leading,-16)
-                               
                             Label("By Buzzer attendance students will response to mark attendance", systemImage: "")
                                 .font(Font.system(size: 12))
                                 .foregroundColor(.gray)
                                 .padding(.leading,10)
                                 .padding(.top,-3)
-                           
                         }
                     }
-
-                   
-                    
                 }
                 Spacer()
-                
                 Button {
-                    
                 } label: {
                     Label("", systemImage: "chevron.forward")
                         .foregroundColor(.black)
                         .padding()
                 }
-                
             }
-            
             Divider()
-
             HStack{
                 VStack{
                     Button {
-                        
                     } label: {
                         VStack{
                             Label("Buzzer",systemImage: "bell.fill")
@@ -318,29 +246,19 @@ struct AttendanceView: View {
                                 .padding(.top,-3)
                         }
                     }
-
-                   
-                   
-                    
                 }
                 Spacer()
-                
                 Button {
-                    
                 } label: {
                     Label("", systemImage: "chevron.forward")
                         .foregroundColor(.black)
                         .padding()
                 }
-                
             }
-            
             Divider()
-            
             HStack{
                 VStack{
                     Button {
-                        
                     } label: {
                         VStack{
                             Label("Surprise Quiz",systemImage: "questionmark.circle.fill")
@@ -351,108 +269,76 @@ struct AttendanceView: View {
                                 .foregroundColor(.gray)
                                 .padding(.leading,10)
                                 .padding(.top,-3)
-                        
                         }
                     }
-
-                   
-                    
                 }
-             Spacer()
-                
+                Spacer()
                 Button {
-                    
                 } label: {
                     Label("", systemImage: "chevron.forward")
                         .foregroundColor(.black)
                         .padding()
                 }
-                
             }
-            
             Divider()
-            
         }
         .padding(.top,-20)
-        
-    
     }
     
     //MARK: ->frontView
-   @ViewBuilder var frontView: some View{
-       
-       VStack {
-          
-           VStack{
-               HStack{
-                   Label("Session Details", systemImage: "")
-                   Spacer()
-                   ZStack{
-                       Button {
-                           
-                       } label: {
-                           
-                           Text("More Info")
-                           Image(systemName: "info.circle")
-                           
-                           
-                       }
-                   }
-                   .frame(width: 120,height: 30)
-                  
-                   .overlay( /// apply a rounded border
-                       RoundedRectangle(cornerRadius: 20)
-                           .stroke(.blue, lineWidth: 2))
-                   .padding(.trailing,10)
-
-                   
-               }
-               
-               .padding(.top,100)
-               Divider()
-               VStack{
-                   Label("Sessoin timing", systemImage: "")
-                       .font(Font.system(size: 12))
-                       .foregroundColor(.gray)
-                       .padding(.trailing,250)
-                   Label("7 Jan 10:00AM-11:00AM", systemImage: "")
-                       .padding(.trailing,140)
-                       .padding(.top,-3)
-                   Label("Student group", systemImage: "")
-                       .font(Font.system(size: 12))
-                       .foregroundColor(.gray)
-                       .padding(.trailing,250)
-                       .padding(.top,5)
-                   Label("MG1 - G1( 50 Students )", systemImage: "")
-                       .padding(.trailing,140)
-                       .padding(.top,-3)
-                   
-                   
-               }
-               
-           }
-           popUpView
-           
-               .frame(width: 400,height: 400)
-               .background(Color(uiColor: .white))
-               .cornerRadius(15)
-             //  .padding(.top,10)
-               
-             Spacer()
-          
-               
-       }
-      
+    @ViewBuilder var frontView: some View{
+        VStack {
+            VStack{
+                HStack{
+                    Label("Session Details", systemImage: "")
+                    Spacer()
+                    ZStack{
+                        Button {
+                        } label: {
+                            Text("More Info")
+                            Image(systemName: "info.circle")
+                        }
+                    }
+                    .frame(width: 120,height: 30)
+                    .overlay( /// apply a rounded border
+                        RoundedRectangle(cornerRadius: 20)
+                            .stroke(.blue, lineWidth: 2))
+                    .padding(.trailing,10)
+                }
+                .padding(.top,100)
+                Divider()
+                VStack{
+                    Label("Sessoin timing", systemImage: "")
+                        .font(Font.system(size: 12))
+                        .foregroundColor(.gray)
+                        .padding(.trailing,250)
+                    Label("7 Jan 10:00AM-11:00AM", systemImage: "")
+                        .padding(.trailing,140)
+                        .padding(.top,-3)
+                    Label("Student group", systemImage: "")
+                        .font(Font.system(size: 12))
+                        .foregroundColor(.gray)
+                        .padding(.trailing,250)
+                        .padding(.top,5)
+                    Label("MG1 - G1( 50 Students )", systemImage: "")
+                        .padding(.trailing,140)
+                        .padding(.top,-3)
+                }
+            }
+            popUpView
+                .frame(width: 400,height: 400)
+                .background(Color(uiColor: .white))
+                .cornerRadius(15)
+            Spacer()
+        }
     }
-    
- 
 }
 
 
 //MARK: ->ContentView_Previews
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-    AttendanceView()
+        AttendanceView()
     }
 }
 
@@ -473,7 +359,7 @@ extension Color {
         default:
             (a, r, g, b) = (1, 1, 1, 0)
         }
-
+        
         self.init(
             .sRGB,
             red: Double(r) / 255,
